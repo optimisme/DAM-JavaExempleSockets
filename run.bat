b0d1de746ff8432a95717eb3878954f634557b43
optimisme <optimisme@gmail.com>
2023-05-24 13:29:27 +0200

    Create .jar

--- run.bat
+++ run.bat
@@ -1,11 +1,43 @@
 rem run with: .\run.bat
 
 cls
-rm -r -force .\bin
-rm -r -force .\src\.*
-rm -r -force .\lib\javafx-windows\lib\.*
-mkdir bin
-xcopy .\assets .\bin\assets /E /I /Y
 
-javac -cp ".;.\\bin;.\\lib\\Java-WebSocket-1.5.3.jar;.\\lib\\slf4j-api-2.0.3.jar;.\\lib\\slf4j-simple-2.0.3.jar" -d .\bin\ .\src\*.java
-java -cp ".;.\\bin;.\\lib\\Java-WebSocket-1.5.3.jar;.\\lib\\slf4j-api-2.0.3.jar;.\\lib\\slf4j-simple-2.0.3.jar" Main
+REM Remove any existing Project.jar file
+del Project.jar
+
+REM Remove any existing .class files from the bin directory
+del /Q bin\*.*
+
+REM Create the bin directory if it doesn't exist
+mkdir bin
+
+REM Copy the assets directory to the bin directory
+xcopy /E /I assets bin\assets
+
+REM Generate the CLASSPATH by iterating over JAR files in the lib directory and its subdirectories
+set "lib_dir=lib"
+setlocal enabledelayedexpansion
+set "class_path="
+
+REM Find all JAR files in the lib directory and its subdirectories
+for /R "%lib_dir%" %%F in (*.jar) do (
+  set "class_path=!class_path!;%%F"
+)
+
+REM Remove the leading ';' from the class_path
+set "class_path=!class_path:~1!"
+
+REM Compile the Java source files and place the .class files in the bin directory
+javac -d bin/ src/*.java -cp "%class_path%"
+
+REM Create the Project.jar file with the specified manifest file and the contents of the bin directory
+jar cfm Project.jar src/Manifest.txt -C bin .
+
+REM Remove any .class files from the bin directory
+del /Q bin\*.*
+rd bin
+
+REM Run the Project.jar file
+java -jar Project.jar -cp "%class_path%"
+
+endlocal
--- run.sh
+++ run.sh
@@ -1,23 +1,43 @@
 #!/bin/bash
 
-# Comprimir amb: folder=`basename "$PWD"` && zip -r ../$folder.zip . -x '**/.*' -x '**/__MACOSX' -x '*.zip'
-
 reset
+
+# Remove any existing Project.jar file
+rm Project.jar
+
+# Remove any existing .class files from the bin directory
 rm -f ./bin/*.* 
+
+# Create the bin directory if it doesn't exist
 mkdir -p ./bin
+
+# Copy the assets directory to the bin directory
 cp -r ./assets ./bin
 
-if [[ $OSTYPE == 'linux-gnu' ]]; then
-    javac -cp "./:./bin:./lib/Java-WebSocket-1.5.3.jar:./lib/slf4j-api-2.0.3.jar:./lib/slf4j-simple-2.0.3.jar" -d ./bin/ ./src/*.java
-    java  -cp "./:./bin:./lib/Java-WebSocket-1.5.3.jar:./lib/slf4j-api-2.0.3.jar:./lib/slf4j-simple-2.0.3.jar" Main
-fi
+# Generate the CLASSPATH by iterating over JAR files in the lib directory and its subdirectories
+lib_dir="lib"
+jar_files=()
 
-if [[ $OSTYPE == 'darwin'* ]] && [[ $(arch) == 'i386' ]]; then
-    javac -cp "./:./bin:./lib/Java-WebSocket-1.5.3.jar:./lib/slf4j-api-2.0.3.jar:./lib/slf4j-simple-2.0.3.jar" -d ./bin/ ./src/*.java
-    java  -cp "./:./bin:./lib/Java-WebSocket-1.5.3.jar:./lib/slf4j-api-2.0.3.jar:./lib/slf4j-simple-2.0.3.jar" Main
-fi
+# Find all JAR files in the lib directory and its subdirectories
+while IFS= read -r -d '' jar_file; do
+  jar_files+=("$jar_file")
+done < <(find "$lib_dir" -name "*.jar" -type f -print0)
 
-if [[ $OSTYPE == 'darwin'* ]] && [[ $(arch) == 'arm64' ]]; then
-    javac -cp "./:./bin:./lib/Java-WebSocket-1.5.3.jar:./lib/slf4j-api-2.0.3.jar:./lib/slf4j-simple-2.0.3.jar" -d ./bin/ ./src/*.java
-    java  -cp "./:./bin:./lib/Java-WebSocket-1.5.3.jar:./lib/slf4j-api-2.0.3.jar:./lib/slf4j-simple-2.0.3.jar" Main
-fi
+# Join the JAR files into the class_path
+class_path=$(IFS=:; echo "${jar_files[*]}")
+
+# Remove the leading ':' from the class_path
+export CLASSPATH=${class_path#:}
+
+# Compile the Java source files and place the .class files in the bin directory
+javac -d ./bin/ ./src/*.java -cp $CLASSPATH
+
+# Create the Project.jar file with the specified manifest file and the contents of the bin directory
+jar cfm Project.jar ./src/Manifest.txt -C bin . 
+
+# Remove any .class files from the bin directory
+rm -f ./bin/*.* 
+rmdir ./bin
+
+# Run the Project.jar file
+java -jar Project.jar -cp $CLASSPATH
